eeab32d41f73e419c9971c7c1b39b5dba80f5cf4
biyanakbar <biyan96akbar@gmail.com>
2020-12-01 10:23:29 +0700

    update style content in home-screen

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
-    <v-container>
+    <!-- <v-container> -->
       <v-layout column wrap>
         <v-flex sm6 xs12 md6 lg3 >
           <v-card color="#d4d4d4">
             <v-card-title><h3>Selamat Datang di Sistem Aplikasi Pelaporan</h3></v-card-title>
-            <v-card-text><p class="black--text">ini adalah sistem untuk merekap seluruh laporan gangguan yang berkaitan dengan ideologi, politik, ekonomi, sosial, dan budaya di lingkungan Kabupaten Tangerang.</p></v-card-text>
+            <v-card-text><h5 class="black--text">Sistem aplikasi pelaporan ini adalah sistem untuk merekap seluruh laporan gangguan yang berkaitan dengan ideologi, politik, ekonomi, sosial, dan budaya di lingkungan Kabupaten Tangerang.</h5></v-card-text>
             <v-card-actions>
               <v-btn
                 color="primary"
@@ -36,17 +36,18 @@
                     readonly
                     v-bind="attrs"
                     v-on="on"
+                    style="width:240px;"
                   ></v-text-field>
                 </template>
                 <v-date-picker v-model="dates" scrollable range>
                   <v-spacer></v-spacer>
-                  <v-btn text color="success" @click="dates = []" >
+                  <v-btn text color="primary" @click="dates = []" >
                     Ulang
                   </v-btn>
                   <v-btn text color="error" @click="modal = false" >
                     Batal
                   </v-btn>
-                  <v-btn text color="primary" @click="$refs.dialog.save(dates)" >
+                  <v-btn text color="success" @click="$refs.dialog.save(dates)" >
                     OK
                   </v-btn>
                 </v-date-picker>
@@ -56,14 +57,12 @@
         </div>
         
         <div style="flex-direction: row; flex-wrap: wrap; display: flex;">
-          <v-flex v-for="person in team" :key="person.count_report" >
-            <v-card class="text-center ma-3" router :to="person.route">
-              <v-responsive class="pt-4">
-                <v-icon x-large >mdi-send mdi-rotate-315</v-icon>
-              </v-responsive>
+          <v-flex v-for="category in categories" :key="category.count_report" >
+            <v-card class="text-center ma-3" router :to="category.route" style="text-decoration: none;">
+              <div><v-icon x-large :color="category.color">mdi-send mdi-rotate-315</v-icon></div>
               <v-card-text>
-                <div class="subheading">{{person.count_report}}</div>
-                <div class="grey--text">{{person.category}}</div>
+                <h5 style="color: black; font-weight: bold;">{{category.count_report}}</h5>
+                <h5 style="color: black; font-weight: 300;">{{category.category}}</h5>
               </v-card-text>
             </v-card>
           </v-flex>
@@ -71,9 +70,8 @@
       </div>
       
       <div style="flex-direction: row; flex-wrap: wrap; display: flex; border-radius: 5px; justify-content: space-between;">
-        <div class="table-container">
-          <v-card>
-            <v-card-title style="background: blue;"><v-icon>mdi-message-alert</v-icon>Laporan Terbaru</v-card-title>
+        <div v-for="tableData in tables" :key="tableData.title" class="table-container">
+            <v-card-title><v-icon class="mr-2">{{tableData.icon}}</v-icon>{{tableData.title}}</v-card-title>
             <v-data-table
               style="background: #d4d4d4;"
               dense
@@ -82,27 +80,13 @@
               :hide-default-footer="true"
               :items="reports"
               item-key="name"
-              class="elevation-1"
-            ></v-data-table>
-          </v-card>
-        </div>
-        <div class="table-container">
-          <v-card>
-            <v-card-title style="background: blue;"><v-icon>mdi-message-processing</v-icon>Laporan Sedang Diproses</v-card-title>
-            <v-data-table
-              style="background: #d4d4d4;"
-              dense
-              :headers="headers"
-              :hide-default-header="true"
-              :hide-default-footer="true"
-              :items="reports"
-              item-key="name"
-              class="elevation-1"
-            ></v-data-table>
-          </v-card>
+            ><router-link to="/lihat-laporan"></router-link></v-data-table>
+            <v-card-actions>
+              <v-btn text color="primary" right="true" :to="tableData.route">Selengkapnya ...</v-btn>
+            </v-card-actions>
         </div>
       </div>
-    </v-container>
+    <!-- </v-container> -->
 
   </div>
 </template>
@@ -126,12 +110,12 @@ export default {
         { first_name: 'Geneva' },
         { first_name: 'Jami' }
       ],
-      team: [
-        {count_report: '70', category: 'Ideologi', route: '/lihat-laporan'},
-        {count_report: '15', category: 'Politik', route: '/lihat-laporan'},
-        {count_report: '9', category: 'Ekonomi', route: '/lihat-laporan'},
-        {count_report: '19', category: 'Sosial', route: '/lihat-laporan'},
-        {count_report: '10', category: 'Budaya', route: '/lihat-laporan'},
+      categories: [
+        {count_report: '70', category: 'Ideologi', route: '/lihat-laporan', color: 'blue'},
+        {count_report: '15', category: 'Politik', route: '/lihat-laporan', color: 'yellow'},
+        {count_report: '9', category: 'Ekonomi', route: '/lihat-laporan', color: 'green'},
+        {count_report: '19', category: 'Sosial', route: '/lihat-laporan', color: 'orange'},
+        {count_report: '10', category: 'Budaya', route: '/lihat-laporan', color: 'red'},
       ],
       reports: [
         {
@@ -165,6 +149,10 @@ export default {
           name: 'KitKat',
         },
       ],
+      tables: [
+        {title: 'Laporan Terbaru', icon: 'mdi-message-alert', route: '/lihat-laporan'},
+        {title: 'Laporan Sedang Diproses', icon: 'mdi-message-processing', route: '/lihat-laporan'},
+      ],
       headers: [
         {
           text: 'Dessert (100g serving)',
@@ -189,15 +177,19 @@ export default {
 <style>
 @media screen and (max-width:2000px){
     .table-container {
-      width: 49%;
-      margin-top: 10px; 
+      background: #d4d4d4;
+      border-radius: 5px;
+      width: 49.5%;
+      margin-top: 10px;
     }
 }
 
 @media screen and (max-width:958px){
     .table-container {
+      background: #d4d4d4;
+      border-radius: 5px;
       width: 100%;
-      margin-top: 10px; 
+      margin-top: 10px;
     }
 }
 </style>
