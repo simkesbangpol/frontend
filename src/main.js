1e34b1d8728b2c88129a1fc9b89ee1f358f7bee9
biyanakbar <biyan96akbar@gmail.com>
2020-12-02 09:30:44 +0700

    remove import bootstrap and css file

--- src/main.js
+++ src/main.js
@@ -1,15 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
-
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-import './assets/css/main.css'
 import vuetify from './plugins/vuetify';
-
-Vue.use(BootstrapVue)
-Vue.use(BootstrapVueIcons)
 
 Vue.config.productionTip = false
 
