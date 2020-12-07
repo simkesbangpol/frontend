e39f3fef8f47cfec868854ffc69fcac6ad1bc77c
biyanakbar <biyan96akbar@gmail.com>
2020-12-07 20:39:39 +0700

    add vuex

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import vuetify from './plugins/vuetify';
+import vuetify from './plugins/vuetify'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
 
 Vue.config.productionTip = false
 
