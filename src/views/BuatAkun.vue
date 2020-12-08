39a3edc57faf4b4f019fcbb816842f244075faf6
biyanakbar <biyan96akbar@gmail.com>
2020-12-08 12:06:11 +0700

    add buat-akun dan detail-akun page

---
+++ src/views/BuatAkun.vue
@@ -0,0 +1,81 @@
+<template>
+  <v-container fluid>
+    <v-row>
+      <v-col>
+        <v-form
+          ref="form"
+          @submit.prevent="submit"
+        >
+          <h3 class="mb-5"><a @click="navigateToKelolaAkun">Data Akun </a>/ Form Buat Akun</h3>
+          
+          <v-row class="ml-1">
+            <v-text-field prepend-icon="mdi-home-city" label="Nama" outlined></v-text-field>
+          </v-row>
+          <v-row class="ml-1">
+            <v-text-field prepend-icon="mdi-map-marker" label="Alamat" style="width: 100%" outlined></v-text-field>
+            <v-select
+              prepend-icon="mdi-blank"
+              style="margin-bottom: 30px;"
+              label="Kecamatan"
+              outlined
+              v-model="e1"
+              :items="states"
+              menu-props="auto"
+              hide-details
+            ></v-select>
+            <v-select
+              prepend-icon="mdi-blank"
+              style="margin-bottom: 30px;"
+              label="Kelurahan"
+              outlined
+              v-model="e2"
+              :items="states"
+              menu-props="auto"
+              hide-details
+            ></v-select>
+          </v-row>
+          <v-row class="ml-1">
+            <v-text-field prepend-icon="mdi-phone-classic" label="No Telp" outlined></v-text-field>
+          </v-row>
+          <v-row class="ml-1">
+              <v-text-field prepend-icon="mdi-email-plus" label="Email" outlined></v-text-field>
+          </v-row>
+
+          <v-row class="ml-1" style="margin-top: 90px;">
+            <v-text-field prepend-icon="mdi-account-key" label="Username" outlined></v-text-field>
+          </v-row>
+          <v-row class="ml-1">
+            <v-text-field prepend-icon="mdi-key-variant" type="password" label="Password" outlined></v-text-field>
+          </v-row>
+
+          <v-row>
+            <div style="width: 100%; display: flex; justify-content: flex-end;">
+              <v-btn color="primary" width="13%">
+                Simpan
+              </v-btn>
+            </div>
+          </v-row>
+        </v-form>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+    name: 'BuatAkun',
+    components: {
+    },
+    data () {
+        return {
+        }
+    },
+    methods: {
+        navigateToKelolaAkun() {
+            this.$router.push({ path: "/kelola-akun" });
+        }
+    },
+}
+</script>
---
+++ src/views/DetailAkun.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-container fluid>
+    <v-row no-gutters>
+        <v-col cols="12">
+            <h3 class="subheading black--text"><a @click="navigateToKelolaAkun">Data Akun </a> / Detail Akun</h3>
+        </v-col>
+    </v-row>
+    <v-row>
+        <v-col cols="12">
+            <v-simple-table>
+                <template v-slot:default>
+                    <tbody>
+                        <tr
+                        v-for="item in detailAkun"
+                        :key="item.title"
+                        >
+                            <td style="background: #e3e3e3;">{{ item.title }}</td>
+                            <td>{{ item.desc }}</td>
+                        </tr>
+                    </tbody>
+                </template>
+            </v-simple-table>
+        </v-col>
+    </v-row>
+    <v-row no-gutters align="baseline">
+        <v-col cols="11"/>
+        <v-col md="1" cols="12">
+            <v-btn color="primary">Ubah</v-btn>
+        </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+    name: 'DetailAkun',
+    components: {
+    },
+    data () {
+        return {
+            detailAkun: [
+                {title: "Nama", desc:"test Nama"},
+                {title: "Alamat", desc:"test Alamat"},
+                {title: "No Telp", desc:"test Telp"},
+                {title: "Email", desc:"test Email"},
+                {title: "Username", desc:"test Username"},
+                {title: "Password", desc:"test Password"},
+            ],
+        }
+    },
+    methods: {
+        navigateToKelolaAkun() {
+            this.$router.push({ path: "/kelola-akun" });
+        }
+    },
+}
+</script>
