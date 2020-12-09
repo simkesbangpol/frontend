231198cdeb62b71c9d70c737f18c1575ba5256ab
Irfan Muhammad Abyan <irfanabyan96@gmail.com>
2020-12-09 21:31:12 +0700

    Implementasi pemanggilan API.

---
+++ src/store/index.js
@@ -0,0 +1,62 @@
+import Vue from 'vue';
+import Vuex from 'vuex'
+import axios from 'axios'
+import createPersistedState from "vuex-persistedstate";
+
+Vue.use(Vuex)
+
+const base_url = "http://127.0.0.1:8000/";
+
+const getDefaultState = () => {
+    return {
+        token: '',
+        permissions: [],
+        user: null
+    }
+}
+
+const stores = {
+    state: getDefaultState(),
+    getters: {
+        getUserToken: state => {
+            return state.token
+        },
+
+        getUser: state => {
+            return state.user
+        },
+
+        getUserPermissions: state => {
+            return state.permissions
+        },
+    },
+    mutations: {
+        login(state, payload){
+            axios.post(base_url+'auth',{
+                username: payload.username,
+                password: payload.password
+            }).then(response => {
+                if(response.status === 200){
+                    state.user = response.data.user
+                    state.token = response.data.access_token
+                    state.permissions = response.data.permissions
+                }
+            })
+        },
+        logout(state){
+            state.user = null
+            state.token = ''
+            state.permissions = []
+        }
+    },
+    actions: {
+        userLogin({ commit }, payload) {
+            commit('login', payload)
+        }
+    },
+    plugins: [createPersistedState({
+        storage: window.sessionStorage,
+    })]
+}
+
+export const store = new Vuex.Store(stores)
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -8,14 +8,14 @@
         >
           <h3 class="mb-5">Login</h3>
           <v-row class="ml-1">
-            <v-text-field prepend-icon="mdi-comma" label="Username" outlined></v-text-field>
+            <v-text-field prepend-icon="mdi-comma" label="Username" outlined v-model="username"></v-text-field>
           </v-row>
           <v-row class="ml-1">
-            <v-text-field prepend-icon="mdi-comma" label="Password" outlined></v-text-field>
+            <v-text-field prepend-icon="mdi-comma" label="Password" outlined v-model="password"></v-text-field>
           </v-row>
           <v-row>
             <div style="width: 100%; display: flex; justify-content: flex-end;">
-              <v-btn color="primary" width="13%">
+              <v-btn color="primary" width="13%" @click="login">
                 Login
               </v-btn>
             </div>
@@ -32,6 +32,17 @@
 export default {
   name: 'Login',
   components: {
+  },
+  data() {
+    return {
+      username: '',
+      password: '',
+    }
+  },
+  methods: {
+    login(){
+      this.$store.dispatch('userLogin', {username: this.username, password: this.password})
+    }
   }
 }
 </script>
