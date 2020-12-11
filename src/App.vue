99395f288c5d97139e2a3de21f0ff78f85a4ee7c
biyanakbar <biyan96akbar@gmail.com>
2020-12-11 22:01:56 +0700

    add footer

--- src/App.vue
+++ src/App.vue
@@ -4,17 +4,20 @@
     <v-content class="ma-16">
       <router-view class="mt-5"></router-view>
     </v-content>
+    <Footer v-if="this.$store.getters.isLoggedIn" />
   </v-app>
 </template>
 
 <script>
 
 import Navbar from '@/components/Navbar'
+import Footer from '@/components/Footer'
 export default {
   name: 'App',
 
   components: {
    Navbar,
+   Footer
   },
 
   data: () => ({
---
+++ src/components/Footer.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-footer padless dark>
+    <v-col
+      class="text-center"
+      cols="12"
+    >
+      <small>Copyright &copy; {{ new Date().getFullYear() }} — Badan Kesbangpol Tangerang</small>
+    </v-col>
+  </v-footer>
+</template>
